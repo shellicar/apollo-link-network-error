9f6658f49eadf908649dc1479399575a42a54d81
Stephen Hellicar <shellicar@gmail.com>
2023-07-23 17:34:13 +1000

    Fix to not try to get cache for mutations.

--- src/handler/cacheFirstHandler.ts
+++ src/handler/cacheFirstHandler.ts
@@ -1,5 +1,6 @@
-import { DocumentNode } from '@apollo/client/core';
+import { DocumentNode, Operation } from '@apollo/client/core';
 import { INetworkResponse } from '../NetworkErrorLink'
+import { OperationDefinitionNode } from 'graphql';
 
 interface Query<TVariables> {
   query: DocumentNode
@@ -29,18 +30,19 @@ export const cacheFirstHandler = (cache: ICacheShape) => ({
   operation,
 }: INetworkResponse) => {
   const skipFlag = operation.getContext().__skipErrorAccordingCache__;
-  if (!skipFlag) {
-    throw error
+  if (skipFlag) {
+    const opDefinition = operation.query.definitions[0];
+    if (opDefinition.kind === 'OperationDefinition') {
+      if (opDefinition.operation === 'query') {
+        const result = cache.read<any>({
+          query: operation.query,
+          variables: operation.variables,
+        })
+        if (result != null) {
+          return result;
+        }
+      }
+    }
   }
-
-  const result = cache.read<any>({
-    query: operation.query,
-    variables: operation.variables,
-  })
-
-  if (!result) {
-    throw error
-  }
-
-  return result
+  throw error
 }
